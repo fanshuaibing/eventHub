18559ea9cddc1fc42af55fd39fdd64f00c6a8edc
fatfan <fatfan1020@gmail.com>
2020-05-24 23:12:51 +0800

    feat: ts 手写 eventHub

---
+++ eventHub/index.ts
@@ -0,0 +1,38 @@
+const eventHub = {
+  cache: {},
+  on(eventName: string, fn: EventHubFn) {
+    this.cache[eventName] = this.cache[eventName] || [];
+    this.cache[eventName].push(fn);
+  },
+  emit(eventName: string, data?: unknown) {
+    (this.cache[eventName] || []).forEach((fn) => {
+      fn.call(data);
+    });
+  },
+  off(eventName: string, fn: EventHubFn) {
+    this.cache[eventName] = this.cache[eventName] || [];
+    let index = indexOf(this.cache[eventName], fn);
+    console.log(index);
+    if (index === -1) {
+      return;
+    }
+    this.cache[eventName].splice(index, 1);
+  },
+};
+
+interface EventHubFn {
+  (data: unknown): void;
+}
+
+function indexOf(arr, fn) {
+  let index = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === fn) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
+
+export default eventHub;
